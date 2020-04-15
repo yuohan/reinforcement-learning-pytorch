8b078cef57c730387d4b346e4781a770d7c4fdf6
Yuohan Lee <derq3278@gmail.com>
2020-04-15 10:33:59 -0700

    Encapsulate to agent and policy
    Policy class handles epsilon calculation and action selection parts.
    Agent class updates q networks.

--- dqn.py
+++ dqn.py
@@ -2,11 +2,15 @@ import gym
 import math
 import copy
 import random
+from collections import namedtuple
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+
+Transition = namedtuple('Transition', 
+    ('obs', 'action', 'reward', 'next_obs', 'done'))
 
 class ReplayBuffer:
 
@@ -20,44 +24,106 @@ class ReplayBuffer:
         if len(self.memory) > self.size:
             self.memory.pop(0)
         
-        self.memory.append((obs, action, reward, next_obs, int(done)))
+        self.memory.append(Transition(obs, action, reward, next_obs, int(done)))
 
     def sample(self, batch_size):
 
         if len(self.memory) < batch_size:
             raise ValueError("Memory size is smaller than batch size")
 
-        return tuple(zip(*random.sample(self.memory, batch_size)))
+        return Transition(*zip(*random.sample(self.memory, batch_size)))
 
-class DQN(nn.Module):
+class EpsilonGreedy:
+    """
+    Epsilon Greedy Policy
+    """
+    def __init__(self, agent, num_actions, init_epsilon, final_epsilon, epsilon_decay):
 
-    def __init__(self, obs_dim, num_actions, hidden_units):
-        super().__init__()
+        self.agent = agent
+        self.num_actions = num_actions
         
-        self.hidden1 = nn.Linear(obs_dim, hidden_units)
-        self.hidden2 = nn.Linear(hidden_units, hidden_units)
-        self.output = nn.Linear(hidden_units, num_actions)
+        self.init_epsilon = init_epsilon
+        self.final_epsilon = final_epsilon
+        self.epsilon_decay = epsilon_decay
 
-    def forward(self, x):
+    def act(self, obs, t):
 
-        x = F.relu(self.hidden1(x))
-        x = F.relu(self.hidden2(x))
-        x = self.output(x)
+        epsilon = self.final_epsilon + (self.init_epsilon - self.final_epsilon) * math.exp(-1. * t / self.epsilon_decay)
+        
+        if random.random() > epsilon:
+            with torch.no_grad():
+                action = self.agent.get_q(torch.Tensor(obs).unsqueeze(0)).max(1)[1].item()
+        else:
+            action = random.randrange(self.num_actions)
 
-        return x
+        return action
+
+class DQN():
+    """
+    DQN (Deep Q Network) Agent Class
+    """
+    def __init__(self, device, obs_dim, num_actions, hidden_units, lr, gamma=0.99):
+
+        self.device = device
+
+        self.q_net = nn.Sequential(
+            nn.Linear(obs_dim, hidden_units),
+            nn.ReLU(),
+            nn.Linear(hidden_units, hidden_units),
+            nn.ReLU(),
+            nn.Linear(hidden_units, num_actions)
+        )
+
+        self.target_net = copy.deepcopy(self.q_net)
+
+        self.criterion = nn.MSELoss(reduction='mean')
+        self.optimizer = optim.Adam(self.q_net.parameters(), lr=lr)
+
+        self.gamma = gamma
+
+    def train(self, transitions):
+
+        obs = torch.tensor(transitions.obs, dtype=torch.float32).to(self.device)
+        action = torch.tensor(transitions.action).to(self.device)
+        reward = torch.tensor(transitions.reward).to(self.device)
+        next_obs = torch.tensor(transitions.next_obs, dtype=torch.float32).to(self.device)
+        done = torch.tensor(transitions.done).to(self.device)
+
+        # Q(s_t, a)
+        q = self.q_net(obs).gather(1, action.unsqueeze(1)).squeeze(1)
+        # y_t = r_t                                         if episode terminate at step t+1
+        #       r_t + gamma * max_{a'} Q_targ(s_{t+1}, a')  otherwise
+        y = reward + (self.gamma * self.target_net(next_obs).max(-1)[0])*(1-done)
+        # MSE
+        loss = self.criterion(q, y)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return loss.item()
+
+    def get_q(self, obs):
+        with torch.no_grad():
+            return self.q_net(obs)
+
+    def update_target(self):
+
+        for param, param_target in zip(self.q_net.parameters(), self.target_net.parameters()):
+                param_target.data.copy_(param.data)
 
 def main():
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     lr = 1e-3
     gamma = 0.99
-    init_explore = 1
-    final_explore = 0.01
-    explore_decay = 200
+    init_epsilon = 1
+    final_epsilon = 0.01
+    epsilon_decay = 200
     seed = 123
 
     batch_size = 128
     memory_size = 500
+    hidden_units = 128
 
     train_timesteps = 15000
     train_start_time = 128
@@ -70,14 +136,11 @@ def main():
 
     obs_dim = env.observation_space.shape[0]
     num_actions = env.action_space.n
-    hidden_units = 128
 
     memory = ReplayBuffer(memory_size)
-    main_net = DQN(obs_dim, num_actions, hidden_units)
-    target_net = copy.deepcopy(main_net)
 
-    criterion = nn.MSELoss(reduction='mean')
-    optimizer = optim.Adam(main_net.parameters(), lr=lr)
+    agent = DQN(device, obs_dim, num_actions, hidden_units, lr, gamma)
+    policy = EpsilonGreedy(agent, num_actions, init_epsilon, final_epsilon, epsilon_decay)
 
     # populate replay memory
     obs = env.reset()
@@ -103,13 +166,7 @@ def main():
     for t in range(1, train_timesteps+1):
 
         # choose action
-        epsilon = final_explore + (init_explore - final_explore) * math.exp(-1. * t / explore_decay)
-        if random.random() > epsilon:
-            with torch.no_grad():
-                action = main_net(torch.Tensor(obs).unsqueeze(0)).max(1)[1].item()
-        else:
-            action = random.randrange(num_actions)
-
+        action = policy.act(obs, t)
         next_obs, reward, done, _ = env.step(action)
         memory.add(obs, action, reward, next_obs, done)
 
@@ -117,23 +174,8 @@ def main():
         
         # sample batch transitions from memory
         transitions = memory.sample(batch_size)
-        
-        batch_obs = torch.tensor(transitions[0], dtype=torch.float32).to(device)
-        batch_a = torch.tensor(transitions[1]).to(device)
-        batch_r = torch.tensor(transitions[2]).to(device)
-        batch_next_obs = torch.tensor(transitions[3], dtype=torch.float32).to(device)
-        batch_d = torch.tensor(transitions[4]).to(device)
-
-        # Q(s_t, a)
-        q = main_net(batch_obs).gather(1, batch_a.unsqueeze(1)).squeeze(1)
-        # y_j = r_j                                         if episode terminate at step j+1
-        #       r_j + gamma * max_{a'} Q_targ(s_{t+1}, a')  otherwise
-        y = batch_r + (gamma * target_net(batch_next_obs).max(-1)[0])*(1-batch_d)
-        # MSE
-        loss = criterion(q, y)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        # train
+        loss = agent.train(transitions)
 
         # record reward
         episode_reward += reward
@@ -143,8 +185,7 @@ def main():
 
         # update target network at every C timesteps
         if t % target_update_frequency == 0:
-            for param_main, param_target in zip(main_net.parameters(), target_net.parameters()):
-                param_target.data.copy_(param_main.data)
+            agent.update_target()
 
         if done:
             # start a new episode
